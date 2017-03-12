c4ad203d8d4aa8dd502afd1d9a31f5b1317d076b
Manuel Colorado <mcolorado@blizzard.com>
2017-03-12 14:11:13 +0000

    working application, no style

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,25 @@
+var axios = require('axios');
+var OpenWeatherKey = require('../../keys.jsx');
+const API_KEY = OpenWeatherKey.module.OpenWeatherKey;
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?&units=metric&appid=';
+
+module.exports= {
+    getTemp: function(location){
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}${API_KEY}&q=${encodedLocation}`;
+        console.log(OpenWeatherKey);
+        console.log(requestUrl);
+
+        return axios.get(requestUrl).then( function(res){ //API success, but city is not guaranteed
+            if (res.data.cod && res.data.message) {
+                throw new Error(res.data.message); //no city was returned
+            } else {
+                return res.data.main.temp; //city returned, temparature return
+            }
+
+        }, function(res){
+            throw new Error(res.data.message); //API fails
+        });
+    }
+}
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,14 +1,40 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
+    getInitialState: function(){
+        return { //default values for testing
+            location: 'Miami',
+            temp: 88
+        }
+    },
+
+    handleSearch: function(location){
+        // this.setState({
+        //     location: location,
+        //     temp: 23
+        // })
+        var that = this;
+        openWeatherMap.getTemp(location).then(function(temp){
+                that.setState({ //.this get lost because chaining functions, we use this=that to keep reference
+                    location: location,
+                    temp: temp
+                })
+            }, function(errorMessage){
+                alert(errorMessage); //TODO
+        });
+    },
+
     render: function(){
+        var {temp, location} = this.state;
+
         return(
             <div>
                 <h3>Weather component</h3>
-                <WeatherForm/>
-                <WeatherMessage/>
+                <WeatherForm onSearch={this.handleSearch}/>
+                <WeatherMessage temp={temp} location={location}/>
             </div>
         );
     }
