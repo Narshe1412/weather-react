1eaa721ea80801af889150c5bf043c79860fc8ac
Manuel Colorado <mcolorado@blizzard.com>
2017-03-13 17:00:17 +0000

    refactoring to arrow function

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,19 +1,15 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = React.createClass({
-    render: function(){
+var Nav = (props) => {
         return (
             <div>
                 <h3>Navigation component</h3>
                 <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather </IndexLink>
                 <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About </Link>
                 <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Examples</Link>
-
             </div>
-            
         )
-    }
-})
+};
 
 module.exports = Nav;
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,10 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-    render: function(){
-        var {temp, location} = this.props;
-
-        return(
-            <h3>It's {temp} on {location} city.</h3>
-        );
-    }
-});
+var WeatherMessage = (props) => {
+    var {temp, location} = props;
+    return(
+        <h3>It's {temp} on {location} city.</h3>
+    );
+};
 
 module.exports = WeatherMessage;
