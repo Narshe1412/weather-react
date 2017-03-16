8fb96093fefa75e7e19fbbebf9b0c07d6a51c170
Manuel Colorado <manuel1412@gmail.com>
2017-03-16 10:03:15 +0000

    css added

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,10 @@ var Examples = require('Examples');
 
 //load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
+
+//styles
+require('style!css!applicationStyles');
+
 $(document).foundation();
 
 ReactDOM.render(
